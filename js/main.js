d08ff3d0f01285bfc10658e7bdf0dbc671acf02d
VegasLeao <VIC0230965@aluno.firjansenaisesi.com.br>
2023-12-22 21:10:21 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -1,66 +1,137 @@
-// Importando o módulo de serviço e tratamento de exceções
-import * as service from './service.js';
-import * as exception from './exception.js';
+// main.js
 
-// Função para carregar os dados da API na tabela
-async function carregarDados() {
-    try {
-        const bandas = await service.obterBandas();
+function adicionar() {
+    var nomeBanda = document.getElementById("nome").value;
+    var generoBanda = document.getElementById("banda").value;
 
-        // Restante do código para preencher a tabela como antes
-        // ...
+    if (nomeBanda && generoBanda) {
+        fetch('http://localhost:3000/bandas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: nomeBanda,
+                author: generoBanda,
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // Atualizar a tabela ou fazer outras ações necessárias após o sucesso
+            adicionarLinhaTabela(data.id, generoBanda, nomeBanda);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('Erro ao adicionar banda. Por favor, tente novamente.');
+        });
 
-    } catch (error) {
-        exception.tratarErro('Erro ao carregar dados da API:', error);
+        limparFormulario();
+    } else {
+        alert("Por favor, preencha todos os campos.");
     }
 }
 
-// Função para adicionar uma banda
-async function adicionar() {
-    let nomeBanda = document.getElementById("nome").value;
-    let genero = document.getElementById("banda").value;
+function adicionarLinhaTabela(id, generoBanda, nomeBanda) {
+    var table = document.getElementById("myTable");
+    var newRow = table.insertRow();
+    newRow.id = "banda" + id;
 
-    if (nomeBanda && genero) {
-        try {
-            await service.adicionarBanda({
-                nomeBanda: nomeBanda,
-                genero: genero,
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+
+    cell1.innerHTML = generoBanda;
+    cell2.innerHTML = nomeBanda;
+}
+
+function atualizar() {
+    var table = document.getElementById("myTable");
+    var selectedRow = table.getElementsByClassName("selected")[0];
+
+    if (selectedRow) {
+        var id = selectedRow.id.substring(5); // Remove o "banda" do ID
+
+        var nomeBanda = document.getElementById("nome").value;
+        var generoBanda = document.getElementById("banda").value;
+
+        if (nomeBanda && generoBanda) {
+            fetch(`http://localhost:3000/posts/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: nomeBanda,
+                    author: generoBanda,
+                }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                // Atualizar a tabela ou fazer outras ações necessárias após o sucesso
+                selectedRow.cells[0].innerHTML = generoBanda;
+                selectedRow.cells[1].innerHTML = nomeBanda;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                alert('Erro ao atualizar banda. Por favor, tente novamente.');
             });
 
-            // Atualizar a tabela após a adição bem-sucedida
-            carregarDados();
-        } catch (error) {
-            exception.tratarErro('Erro ao adicionar banda:', error);
+            limparFormulario();
+        } else {
+            alert("Por favor, preencha todos os campos.");
         }
     } else {
-        alert("Preencha todos os campos do formulário!");
+        alert("Selecione uma banda para atualizar.");
     }
 }
 
-// Função para remover uma banda
-async function remover(index) {
-    try {
-        await service.removerBanda(index);
-
-        // Atualizar a tabela após a remoção bem-sucedida
-        carregarDados();
-    } catch (error) {
-        exception.tratarErro('Erro ao remover banda:', error);
-    }
-}
-
-// Função para atualizar uma banda (adicionar funcionalidade conforme necessário)
-function atualizar() {
-    // Adicione lógica para atualizar uma banda
-}
-
-// Função para remover a banda selecionada (adicionar funcionalidade conforme necessário)
 function removerSelecionado() {
-    // Adicione lógica para remover a banda selecionada
+    var table = document.getElementById("myTable");
+    var selectedRow = table.getElementsByClassName("selected")[0];
+
+    if (selectedRow) {
+        var id = selectedRow.id.substring(5); // Remove o "banda" do ID
+
+        fetch(`http://localhost:3000/posts/${id}`, {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // Atualizar a tabela ou fazer outras ações necessárias após o sucesso
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('Erro ao excluir banda. Por favor, tente novamente.');
+        });
+
+        table.deleteRow(selectedRow.rowIndex);
+        limparFormulario();
+    } else {
+        alert("Selecione uma banda para remover.");
+    }
 }
 
+function limparFormulario() {
+    document.getElementById("nome").value = "";
+    document.getElementById("banda").value = "";
+    document.getElementById("nome").focus();
+}
 
-// Carregar os dados na tabela ao carregar a página
-window.onload = () => {
-    carregarDados();
-};
+document.getElementById("myTable").addEventListener("click", function (event) {
+    var table = event.target.closest("table");
+    if (table && table.id === "myTable") {
+        var rows = table.getElementsByTagName("tr");
+
+        for (var i = 0; i < rows.length; i++) {
+            rows[i].classList.remove("selected");
+        }
+
+        event.target.parentNode.classList.add("selected");
+
+        var selectedRow = event.target.parentNode;
+        document.getElementById("nome").value = selectedRow.cells[1].innerHTML;
+        document.getElementById("banda").value = selectedRow.cells[0].innerHTML;
+    }
+});
--- js/service.js
+++ js/service.js
@@ -1,40 +1,42 @@
-import {
-    tratarErro
-} from "./exception.js"; 
-// URL da sua API
-const apiUrl = 'https://sua-api.com/bandas';
-
-// Função para obter bandas da API
-export async function obterBandas() {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-    return response.json();
-}
-
-// Função para adicionar uma banda à API
-export async function adicionarBanda(banda) {
-    const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(banda),
-    });
-
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-}
-
-// Função para remover uma banda da API
-export async function removerBanda(index) {
-    const response = await fetch(`${apiUrl}/${index}`, {
-        method: 'DELETE',
-    });
-
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-}
+//export const carregueTodosArtistas = async() =>{
+    //try { 
+      //const response = await fetch(//API ou endereço ou a abreviação criada);
+      //handleErrors(response);
+      //return response.json();    
+    //} catch (error) {
+     // console.log('Error >>>', error);    
+   // }
+  //};
+  
+  //export const createArtista = async (artista) => {
+      //fetch ( //API ou endereço ou a abreviação criada, {
+       //   method: "POST",
+         // headers: {
+          //    "Content-Type": "application/json",
+         // },
+        //  body: JSON.stringify(artista),
+     // })
+     // .then((response) => response.json())
+     // .then((data) => console.log("sucesso: ", data))
+     // .catch((error) => console.log("Erro: ", error));
+  //};
+  
+ // export const deleteArtista = async (artista) => {
+      //fetch(//API ou endereço ou a abreviação criada + /${artista.id}, { method: "DELETE" })
+     // .then((response) => response.json())
+     // .then((data) => console.log("sucesso: ", data))
+     // .catch((error) => console.log("Erro: ", error));
+ // };
+  
+  //export const updateArtista = async (artista) => {
+    //  fetch(//API ou endereço ou a abreviação criada + /${artista.id}, {
+        //method: "PATCH",
+       // headers: {
+        //  "Content-Type": "application/json",
+       // },
+      //  body: JSON.stringify(artista),
+      //})
+       // .then((response) => response.json())
+        //.then((data) => console.log("sucesso: ", data))
+      //  .catch((error) => console.log("Erro: ", error));
+   // };
